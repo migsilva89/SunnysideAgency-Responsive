e1c9b570e2231c4eda85f8138dae0e03e35bd03b
migsilva89 <migdrum@gmail.com>
2022-03-20 23:51:55 +0100

    mobile menu toogle fixed

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function clickAction(){
     var menu = document.getElementById("menu")
     menu.classList.toggle("hidden")  
 }
+
+window.onresize = function() {
+    if (this.window.innerWidth >= 800){
+        var menu = document.getElementById("menu")
+        menu.classList.add("hidden")  
+    }
+};
